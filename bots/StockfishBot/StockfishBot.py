de69b3b37de4651ff0babb9ed58171d9964527ad
lyndellmartin <100326282+lyndellmartin@users.noreply.github.com>
2026-04-04 15:58:29 -0700

    Stockfish exe and wrapper

---
+++ bots/StockfishBot/StockfishBot.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+import chess
+import chess.engine
+
+class StockfishBot:
+    def __init__(self):
+        path = Path(__file__).parent / "bin" / "stockfish-windows-x86-64-avx2.exe" # filepath to the stockfish bot
+        self.engine = chess.engine.SimpleEngine.popen_uci(str(path)) # open the exe as an engine
+
+    def choose_move(self, board):
+        return self.engine.play(board, chess.engine.Limit(time=0.2)).move
+
+    def close(self):
+        self.engine.quit()
